ccb07e1b375c736b0676d0a049719ec18886c037
bappy1988 <ben@wardmail.org.uk>
2018-01-05 00:42:30 +0000

    basic exports done

--- index.js
+++ index.js
@@ -1,2 +1,55 @@
 const data = require('./data.json');
-console.log(data);
+
+const validate = (color, weight) => {
+    if (typeof data[color] === 'undefined') {
+        throw "Color '" + color + "' is not recognised. Use a value from 'colors' array.";
+    } else if (weight && typeof data[color][weight] === 'undefined'){
+        throw "Weight '" + weight + "' is not valid for color '" + color + "'.";
+    }
+}
+
+export const colors = Object.keys(data);
+
+export const weights = [
+    'W50',
+    'W100',
+    'W200',
+    'W300',
+    'W400',
+    'W500',
+    'W600',
+    'W700',
+    'W800',
+    'W900',
+    'A100',
+    'A200',
+    'A400',
+    'A700',
+];
+
+export const getTextClass = (color, weight) => {
+    validate(color, weight);
+    if (weight) {
+        return data[color][weight].textClass;
+    } else {
+        return data[color].textClass;
+    }
+};
+
+export const getColorClass = (color, weight) => {
+    validate(color, weight);
+    if (weight) {
+        return data[color][weight].colorClass;
+    } else {
+        return data[color].colorClass;
+    }
+};
+
+export const getHex = (color, weight) => {
+    validate(color, weight);
+    if (weight) {
+        return data[color][weight].hex;
+    } else {
+        return data[color].hex;
+    }
+};
